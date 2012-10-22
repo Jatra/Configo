cf117dd8cfe0a698889df3993520b1108570a501
Tim Russell <tim@inqmobile.com>
2012-10-22 14:24:41 +0100

    More config

--- src/uk/co/jatra/android/configuration/Config.java
+++ src/uk/co/jatra/android/configuration/Config.java
@@ -7,11 +7,31 @@ import java.util.concurrent.Callable;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.util.Log;
+
+/**
+ * Config class that reads configuration information from a known file res/values/config.xml
+ * The config.xml is expected to be modified, or even created at compile time.
+ * eg a -pre-compile target can be added using the task echoxml, see the custom_rules.xml file.
+ * <pre>
+ * Parameters
+    build_num  
+    version  
+    build_type one of the enumerated constants below in BuildType
+    software_type  done of the enumerated constants below in SoftwareType
+    expiry    format must be  Jul 1, 2012  (ie MEDIUM, Locale, US)
+    </pre>
+ *  Note, no expiration action is implicit in this file, it is up to the application to use it.
+ */
 
 public class Config {
     private static Config instance;
     
+
     private static DateFormat sDateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM, Locale.US);
+    
+    /** Unknown build or software type. */
+    public static final int UNKNOWN = 0;
     
     /** Development software - not a formal tracked build. */
     public static final int DEVELOPMENT = 0x1;
@@ -34,42 +54,61 @@ public class Config {
     public static final int AUTO = DEVELOPMENT<<8;
     /** Built using Master */
     public static final int MASTER = DEVELOPMENT<<9;
+
+
+    private static final String TAG = "Config";
     
+    /** The permitted Software types in the config.xml */
     public enum SoftwareType {
+        UNKNOWN,
         EXPERIMENTAL,
         DEVELOPMENT,
         TRIAL,
         PRE_PRELEASE,
         RELEASE
     };
+    /** The permitted Build types in the config.xml */
     public enum BuildType {
+        UNKNOWN,
         ECLIPSE,
         ANT,
         GERRIT,
         AUTO,
         MASTER
     };
+    
     private final SoftwareType mSoftwareType;
     private final BuildType mBuildType;
     private final String mExpiryDate;
     private final long mExpiry;
    
-    
+    /**
+     * private constructor reads from resource files.
+     * Expected that the config values will be in config.xml
+     * 
+     * @param context
+     */
     private Config(Context context) {
         Resources resources = context.getResources();
-        mSoftwareType = SoftwareType.valueOf(resources.getString(R.string.software_type));
-        mBuildType = BuildType.valueOf(resources.getString(R.string.build_type));
+        
+        String softwareType = resources.getString(R.string.software_type);
+        mSoftwareType = softwareType == null ? SoftwareType.UNKNOWN : SoftwareType.valueOf(softwareType);
+        
+        String buildType = resources.getString(R.string.build_type);
+        mBuildType = buildType == null ? BuildType.UNKNOWN : BuildType.valueOf(buildType);
+        
         mExpiryDate = resources.getString(R.string.expiry);
+        Date date = null;
         try {
-            Date date = sDateFormat.parse(mExpiryDate);
-            mExpiry = date.getTime();
+            date = sDateFormat.parse(mExpiryDate);
         } catch (Exception e) {
-            throw new IllegalArgumentException("expiry date invalid"+mExpiryDate);
+            Log.e(TAG, "expiry date invalid"+mExpiryDate+". Using max date");
         }
+        mExpiry = date == null ? Long.MAX_VALUE : date.getTime();
 
     }
     
-    public Config initConfig(Context context) {
+    public static Config initConfig(Context context) {
         if (instance != null) {
             throw new IllegalStateException("Already initialised");
         } else {
@@ -106,7 +145,7 @@ public class Config {
     }
     
     /** 
-     * Test for expiration
+     * Test for expiration, with exception thrown if expired.
      */
     public void checkExpired() {
         if (System.currentTimeMillis() > mExpiry) {
@@ -114,14 +153,28 @@ public class Config {
         }
     }
     
+    /** 
+     * Test for expiration.
+     */
     public boolean isExpired() {
         return System.currentTimeMillis() > mExpiry;
     }
     
+    /**
+     * @param valid String to be returned if not yet expired
+     * @param expired String to be returned if expired
+     * @return whichever String is appropriate according to expiry date.
+     */
     public String getStringPerExpiry(String valid, String expired) {
         return isExpired() ? expired : valid;
     }
     
+    /**
+     * Allow a choice of two different actions according to whether the expiry date has been reached.
+     * The chosen runnable is run synchronously on the calling thread.
+     * @param valid A runnable that is run if the expired date has not been reached.
+     * @param expired A runnable that is run if the expiry date has been reached
+     */
     public void executePerExpriry(Runnable valid, Runnable expired) {
         if (isExpired()) {
             valid.run();
@@ -130,6 +183,12 @@ public class Config {
         }
     }
     
+    /**
+     * Allow a choice of two different actions according to whether the expiry date has been reached.
+     * The chosen Callable is run synchronously on the calling thread.
+     * @param valid A Callable that is run if the expired date has not been reached.
+     * @param expired A Callable that is run if the expiry date has been reached
+     */
     public <V> V executePerExpiry(Callable<V> valid, Callable<V> expired) throws Exception {
         return isExpired() ? expired.call() : valid.call();
     }
